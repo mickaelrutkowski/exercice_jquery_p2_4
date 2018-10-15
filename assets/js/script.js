4ace8ac2a7539f1bafbb143b9fe012eb752363b7
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-10-15 23:07:04 +0200

    exercice 4 partie 2 JQuery

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+  $('#green').click(function() {
+    $('#text').css('color', 'green');
+});
+ $('#red').click(function() {
+   $('#text').css('color', 'red');
+});
+$('#blue').click(function() {
+  $('#text').css('color', 'blue');
+});
+});
+
+//version JP
+// $(function(){
+// $('.color').click(function({//class .color = class="color"
+//   divColor = $(this).attr('id');//attr prend tout les id
+//   $('#text').css('color', divColor)
+// });
+// });
